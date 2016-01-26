3af0af3edde90e2e118b2f1a79173ef5718485e4
Maitland McConnell <maitlandm@gmail.com>
2016-01-26 07:52:51 -0800

    Implemented canvas changes

--- js/game.js
+++ js/game.js
@@ -1,3 +1,24 @@
+
+
+//create the canvas
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext('2d');
+
+
+canvas.width = 1550;
+canvas.height = 960;
+document.body.appendChild(canvas);
+
+var img = new Image();
+img.src = './static/sacredcherrygrove.png';
+img.onload = function() {
+  var pattern = ctx.createPattern(img, 'no-repeat');
+  ctx.fillStyle = pattern;
+  ctx.fillRect(0,0,1550,960);
+};
+
+
+
 function game(building, character){
     this.init = function(){
         var playerOneBuilding = new Building();
@@ -15,31 +36,13 @@ function Building(){
         var white_space = window.innerHeight - (160*number);
         var margin_top = white_space/(number -1);
         console.log(margin_top)
-        // function add_building(number, building){
-        //     if(number > 3){
-        //         //append the first three to the first div part
-        //     }
-        //     number = number -3;
-        //     add_building(number, building);
-        // }
-        // if(number > 3){
-        //     //append the first three
-        //     //append the next three
-        //     number = number -3
 
-        // }
-        // $("#" + building).append('<img class = "castle" src = "./../the_game/static/castle.png"><br>')
+        //if we want to add more buildings, we could have a recursive function here that subtracts 3 from the number as it goes
+
         for(var i = 0; i < number; i++){
             $('#' + building).append('<img class="castle" src="./../the_game/static/castle.png"  style = "margin-top:' + margin_top + '"><br>')
         }
     }
-
-    // var stats = {
-    //     hp: 1,
-    //     flag: false,
-
-    // }
-
 
 }
 
@@ -49,7 +52,7 @@ function Character(){
 
         $('#'+character).css('background', "url('./../the_game/static/link.png') "+x*(0)+"px "+(0 + y)+"px").css(0 + "px");
 
-                // $('#'+selector).css('background', "url('images/ken.png') "+x*(-70)+"px "+(-80*y)+"px").css('left', this.ch_x+"px");
+                $('#'+selector).css('background', "url('images/ken.png') "+x*(-70)+"px "+(-80*y)+"px").css('left', this.ch_x+"px");
 
     }
 }
