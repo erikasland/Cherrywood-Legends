a4cb730085b3f92825a29f6d0efa725f08ffded4
Maitland McConnell <maitlandm@gmail.com>
2016-01-26 15:46:26 -0800

    Link fights it's exciting

--- js/game.js
+++ js/game.js
@@ -11,47 +11,24 @@ var background = new Image();
 background.src = './static/sacredcherrygrove.png';
 
 
-var keysDown = {};
-//EVENT LISTENER FOR THE KEYPRESS
-addEventListener("keydown", function (e){
-    keysDown[e.keyCode] = true;
-}, false);
-//CLEARING THE KEYPRESS
-addEventListener("keyup", function (e){
-    delete keysDown[e.keyCode];
-}, false);
-
-
-//GAME LOOP
-var main = function() {
-    var now = Date.now();
-    var delta = now - then;
-
-    update();
     loadField();
 
-    then = now;
 
-    requestAnimationFrame(main);
-}
-var w = window;
-requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
-var then = Date.now();
-main();
-
-function update(){
-    if(32 in keysDown) {
-
-        for(var i = 0; i <= 5; i++){
-            sprite.draw(i, 600, 650);
-               
-        }
+function reloadField(callback){
+    console.log("reloading")
+    background = new Image();
+    background.src = './static/sacredcherrygrove.png';
+        //load the background
+    background.onload = function() {
+      var pattern = ctx.createPattern(background, 'no-repeat');
+      ctx.fillStyle = pattern;
+      ctx.fillRect(0,0,1550,960);
+      //load the towers and character only after the background has loaded
+      drawTower();
+      callback();
     }
 }
-
-
-
 function loadField() {
     //load the background
     background.onload = function() {
@@ -61,6 +38,7 @@ function loadField() {
       //load the towers and character only after the background has loaded
       drawTower();
       drawCharacter();
+
     };
     
 }
@@ -99,63 +77,162 @@ function drawTower(){
             y = y - 200
             ctx.drawImage(tower, x, y, 176, 276); 
         }
+
     }
 }
 
 
-//draw the character
-function drawCharacter(){
+function drawCharacter() {
+    
+
+
     var character_img = new Image();
+    character_img.onload = function() {
+        character_img_loaded()
+    }
+    // character_img.addEventListener('load', character_img_loaded , false);
     character_img.src = './static/link.png';
-    character_img.onload = function(){
-        sprite.draw(0, 600, 650);
-        
-    }
-    
-    function Sprite(img, width, height, positions){
-        this.img = img;
-        this.width = width;
-        this.height = height;
-        this.positions = positions;
+
+    var positions = [[0,0], [35,0], [70,0], [105,0], [140,0], [175,0]];
+
+    var dx = 35;
+    var dy = 0;
+    var sx = 0;
+    var sy = 0;
+    var destX = 500;
+    var destY = 650;
+
+    function character_img_loaded() {
+
+      console.log("hello")
+      ctx.drawImage(character_img, sx, sy,35,35,destX,destY,35,35);
+
+
     }
 
-    Sprite.prototype = {
-        draw: function(position, x, y){
-            console.log("drawing link")
-            var pos = this.positions[position];
-            ctx.drawImage(
-                this.img,
-                pos[0],
-                pos[1],
-                this.width,
-                this.height,
-                x, 
-                y,
-                this.width,
-                this.height
-                );
-        }
-    }
-    var sprite = new Sprite(character_img, 21, 25, {
-        //walking positions
-        'walk': {'x': [0, 25, 50, 75, 100, 125], 'y': 0}
-    ]);
-   
-    
-}
-
-function attack(){
     $(document).on("keypress", function(e){
         if(e.which == 32){
-            var x = 600;
-            var y = 650;
-            for(var i = 0; i <= 5; i++){
-                sprite.draw(i, x, y);
-               
+            // oldDest = destX;
+            // destX = destX + 35;
+            function fight(){
+                sy = sy+dy;
+                sx = sx+dx;
+                if(sx == 210){
+                    window.clearInterval(start_fight);
+                    sx = 0;
+                }
+                reloadField(function() {
+
+                    ctx.drawImage(character_img, sx, sy, 35, 35,destX,destY,35,35);
+
+                });
             }
+           var start_fight = setInterval(fight, 50);
         }
     })
+
 }
+
+
+var constants = {
+    'WALKING': {
+        'y': 3,
+        'x': [0, 1, 2]
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //draw the character
+// function drawCharacter(){
+//     var constants = {
+//         'WALKING': {
+//             'y': 3,
+//             'x': [0, 1, 2]
+//         }
+//     }
+
+
+//     var character_img = new Image();
+//     character_img.src = './static/link.png';
+//     character_img.onload = function(){
+//         sprite.draw(0, 600, 650);
+        
+//     }
+    
+//     function Sprite(img, width, height, positions){
+//         this.img = img;
+//         this.width = width;
+//         this.height = height;
+//         this.positions = positions;
+//     }
+
+//     Sprite.prototype = {
+//         draw: function(position, x, y){
+//             console.log("drawing link")
+//             var pos = this.positions[position];
+//             ctx.drawImage(
+//                 this.img,
+//                 pos[0],
+//                 pos[1],
+//                 this.width,
+//                 this.height,
+//                 x, 
+//                 y,
+//                 this.width,
+//                 this.height
+//             );
+//         }
+//     }
+//     var sprite = new Sprite(character_img, 21, 25, [
+//         [0,0], [25,0], [50,0]
+//     ]);
+   
+
+//     $(document).on("keypress", function(e){
+//         if(e.which == 32){
+//             var x = 600;
+//             var y = 650;
+//             // for(var i = 0; i <= 2; i++){
+//             //     sprite.draw(i, x, y);
+//             //     x+=25;
+               
+//             // }
+
+            
+//             loadField();
+//             sprite.draw(1, 625, 650);
+//         }
+//     })
+    
+    
+// }
+
+
+
+
 
 
 
