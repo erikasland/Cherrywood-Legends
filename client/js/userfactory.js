348b83f2ac320510fa1e09546d45b62761fabd99
Erik Asland <erikasland87@gmail.com>
2016-01-29 12:14:42 -0800

    added update show and index methods

--- client/js/userfactory.js
+++ client/js/userfactory.js
@@ -1,11 +1,28 @@
 myModule.factory('userFactory', function($http){
     var factory = {};
     factory.user1 = {};
-    factory.user2 = {};
+
+    factory.index = function(callback){
+        $http.get('/users').success(function(output){
+            callback(output);
+        })
+    }
+
     factory.create = function(user1, user2){
         $http.post('/users', {user1: user1, user2: user2}).success(function(output){
-            factory.user1 = output.player_one.name;
-            factory.user2 = output.player_two.name;
+            factory.user1 = output.player_one;
+        })
+    }
+
+    factory.update = function(user){
+        $http.post('/user', user).success(function(output){
+            console.log(output);
+        })
+    }
+
+    factory.show = function(user, callback){
+        $http.get('/user/'+ user._id).success(function(output){
+            callback(output);
         })
     }
     return factory;
--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var UserSchema = new mongoose.Schema({
-    name: String
+    name: String,
+    win: { type: Number, default: 0 }
 })
 mongoose.model('User', UserSchema);
