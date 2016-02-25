1f0916748954d74de1dd51cf19ce628fe68fafee
Erik Asland <erikasland87@gmail.com>
2016-02-25 15:50:24 -0800

    Made site responsive for desktops. Fixed glitch on "enter name" page. Had to resize castles for responsiveness and also so link can move more freely behind the castles

--- client/js/playercontroller.js
+++ client/js/playercontroller.js
@@ -1,13 +1,15 @@
 myModule.controller('playerController', function($location, userFactory){
     var _this = this;  
-        var handler = function(e){
-            if(e.keyCode === 13) {
-                if(_this.new_user.user1){
-                    userFactory.create(_this.new_user.user1);
-                }
-                $location.path('/game')
-            }      
-        };
-        this.docu = angular.element(document);
-        this.docu.on('keydown', handler);
+    this.focus = true;
+    
+    var handler = function(e){
+        if(e.keyCode === 13) {
+            if(_this.new_user.user1){
+                userFactory.create(_this.new_user.user1);
+            }
+            $location.path('/game')
+        }      
+    };
+    this.docu = angular.element(document);
+    this.docu.on('keydown', handler);
 })
